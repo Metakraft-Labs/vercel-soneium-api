16975c24112f747b185a262aa86a63553ac38fe6
D-Dum <piyushankit1410@gmail.com>
2025-05-06 20:18:41 +0530

    Update getBalances.js

--- api/getBalances.js
+++ api/getBalances.js
@@ -3,7 +3,6 @@ const { ethers } = require("ethers");
 const RPC_URL = "https://rpc.ankr.com/somnia_testnet/6e3fd81558cf77b928b06b38e9409b4677b637118114e83364486294d5ff4811"; // 👈 Replace with actual RPC
 
 const CONTRACT_ADDRESSES = [
-  "0xYourToken1",
   "0x0000000000000000000000000000000000000000"
 ];
 
