b5be5a3496d75d3654aace9f4735a018c3a0fe53
D-Dum <piyushankit1410@gmail.com>
2025-05-06 21:04:21 +0530

    Update getBalances.js

--- api/getBalances.js
+++ api/getBalances.js
@@ -3,7 +3,7 @@ const { ethers } = require("ethers");
 const RPC_URL = "https://rpc.ankr.com/somnia_testnet/6e3fd81558cf77b928b06b38e9409b4677b637118114e83364486294d5ff4811";
 const provider = new ethers.JsonRpcProvider(RPC_URL);
 
-// Contract addresses mapped to descriptive variable names
+// Contract addresses aligned with variables
 const contractMap = {
   rpsPurch: "0xD1fC78e743B06F90E2B6A36022763d3E35160E0a",
   rpsScore: "0x89cD2e2124b48737A220b7cA264b12a461e225d3",
@@ -14,7 +14,7 @@ const contractMap = {
   petScoreTotal: "0x754F014dFC79eE5b3bd4335637622Ce03f26bBd9"
 };
 
-// Completion conditions for each key
+// Completion conditions for each Call
 const completionThresholds = {
   rpsPurch: 1000,
   rpsScore: 2500,
